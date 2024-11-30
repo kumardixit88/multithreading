a5fe7748de0cf48b0770ddf88f0726ce3c9df329
Dixit Kumar <dipu@Dixits-Mac-mini.local>
2024-11-30 18:05:14 +0530

    multithreading

---
+++ src/main/java/com/project/multithreading/MultithreadingApplication.java
@@ -0,0 +1,15 @@
+package com.project.multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingApplication {
+
+	public static void main(String[] args) {
+
+		//SpringApplication.run(MultithreadingApplication.class, args);
+		System.out.println("xyz");
+	}
+
+}
